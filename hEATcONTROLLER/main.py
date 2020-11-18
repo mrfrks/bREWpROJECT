1a67f1b30ba25efebb63c39762fd8db776df7c70
Gabor Farkas <mr.gabor.farkas.jr@gmail.com>
2020-11-18 23:19:10 +0100

    First loop trial

--- hEATcONTROLLER/main.py
+++ hEATcONTROLLER/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_me(fam):
+    for height in fam:
+        print(height)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    fam = [1.73, 1.68, 1.71, 1.89]
+    print_me(fam)
