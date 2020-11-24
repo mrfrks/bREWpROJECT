64909683852613e88c8b7124939fd688017ce452
Gabor Farkas <mr.gabor.farkas.jr@gmail.com>
2020-11-24 23:13:32 +0100

    Simple data generator implemented

--- hEATcONTROLLER/main.py
+++ hEATcONTROLLER/main.py
@@ -1,10 +1,20 @@
 
-def print_me(fam):
+#import PySimpleGUI as sg
+
+def print_me(num):
+
     with open('input.txt', 'a') as f:
         f.truncate(0)
-        for height in fam:
-            f.write('%lf,\n' % height)
+        while(1):
+            f.write('%lf,\n' % (num + 0.5))
+            f.write('%lf,\n' % (num + 0.85))
+            if(num + 1 < 100):
+                num = num + 1
+            else:
+                break;
 
 if __name__ == '__main__':
-    fam = [1.73, 1.68, 1.71, 1.89]
-    print_me(fam)
+    num = 19.5
+    print_me(num)
+    #sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+
