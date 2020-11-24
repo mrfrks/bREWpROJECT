d22b5d6e255b0891b1b04c17a09db685895dad67
Gabor Farkas <mr.gabor.farkas.jr@gmail.com>
2020-11-24 22:48:24 +0100

    Basic file creation

--- hEATcONTROLLER/main.py
+++ hEATcONTROLLER/main.py
@@ -1,7 +1,9 @@
 
 def print_me(fam):
-    for height in fam:
-        print(height)
+    with open('input.txt', 'a') as f:
+        f.truncate(0)
+        for height in fam:
+            f.write('%lf,\n' % height)
 
 if __name__ == '__main__':
     fam = [1.73, 1.68, 1.71, 1.89]
